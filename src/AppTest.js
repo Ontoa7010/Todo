3e1594265f85980c575478659ed39be813b64775
minase <ontoa7010@gmail.com>
2020-10-01 19:32:27 +0900

    Clickを押した際、タイトルがTestとなるようにした

--- src/AppTest.js
+++ src/AppTest.js
@@ -1,110 +1,122 @@
 import React , { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+
+function mappingState(state){
+    return state;
+}
 
 class App extends Component{
     render(){
         return <CheckList />;
     }
 }
+//ストアのコネクト
+App = connect()(App);
 
 class CheckList extends Component{
-    data = [
-        "SampleA",
-        "SampleB",
-    ];
     constructor(props){
         super(props);
-        this.state = {
-        title:  'React'
-        };
+        this.doAction = this.doAction.bind(this);
     }
 
+    doAction(e){
+        this.props.dispatch( { type: 'ADD'} );
+    }
     render(){
         return(
         <div>
-            <h1>{this.state.title}</h1>
-            <Form />
+            <h1>{this.props.title}</h1>
+            <ul>
+                {this.props.data.map( (value)=>(
+                    <li>{value}</li>
+                ))};
+            </ul>
+            <button onClick={this.doAction}>Click</button>
+            {/* <Form /> */}
         </div>
         );
     }
 }
 
-class Form extends Component{
-    data =[
-        "SampleA",
-        "SampleB",
-    ];
-    input = '';
+CheckList = connect( mappingState )( CheckList );
 
-    constructor(props){
-        super(props);
-        this.state ={
-            list:       this.data,
-            maxlength:  10
-        }
-        this.doChange = this.doChange.bind(this);
-        this.doSubmit = this.doSubmit.bind(this);
-    }
+// class Form extends Component{
+//     data =[
+//         "SampleA",
+//         "SampleB",
+//     ];
+//     input = '';
 
-    doCheck(event){
-        alert(event.target.value + "は長すぎます。(最大10文字")
-    }
+//     constructor(props){
+//         super(props);
+//         this.state ={
+//             list:       this.data,
+//             maxlength:  10
+//         }
+//         this.doChange = this.doChange.bind(this);
+//         this.doSubmit = this.doSubmit.bind(this);
+//     }
 
-    doChange(event){
-        if(event.target.value.length > this.state.maxlength){
-            this.doCheck(event);
-            event.target.value = event.target.value.substr( 0, this.state.maxlength );
-        }
+//     doCheck(event){
+//         alert(event.target.value + "は長すぎます。(最大10文字")
+//     }
 
-        this.input = event.target.value;
-    }
+//     doChange(event){
+//         if(event.target.value.length > this.state.maxlength){
+//             this.doCheck(event);
+//             event.target.value = event.target.value.substr( 0, this.state.maxlength );
+//         }
 
-    doSubmit(event){
-        this.data.push(this.input);
-        this.setState({
-            list:   this.data
-        });
-        event.preventDefault();
-    }
+//         this.input = event.target.value;
+//     }
 
-    render(){
-        return(
-        <div>
-            <form onSubmit={this.doSubmit} >
-                <input type="text" onChange={this.doChange} size="40" />
-                <input type="submit" value="追加" />
-            </form>
-            <List data={this.state.list}/>
-        </div>
-        );
-    }
-}
+//     doSubmit(event){
+//         this.data.push(this.input);
+//         this.setState({
+//             list:   this.data
+//         });
+//         event.preventDefault();
+//     }
 
-class List extends Component{
-    id = 0;
-    render(){
-        let data = this.props.data;
-        return(
-        <ul>
-            { 
-            data.map( (item) => (
-            <CheckBox value={item}  key={this.id++} />
-            ))}
-        </ul>
-        );
-    }
-}
+//     render(){
+//         return(
+//         <div>
+//             <form onSubmit={this.doSubmit} >
+//                 <input type="text" onChange={this.doChange} size="40" />
+//                 <input type="submit" value="追加" />
+//             </form>
+//             <List data={this.state.list}/>
+//         </div>
+//         );
+//     }
+// }
 
-class CheckBox extends Component{
-    render(){
-        return(
-        <li>
-            <label>
-            <input type="checkbox"/>{this.props.value}
-            </label>
-        </li>
-        );
-    }
-}
+// class List extends Component{
+//     id = 0;
+//     render(){
+//         let data = this.props.data;
+//         return(
+//         <ul>
+//             { 
+//             data.map( (item) => (
+//             <CheckBox value={item}  key={this.id++} />
+//             ))}
+//         </ul>
+//         );
+//     }
+// }
+
+// class CheckBox extends Component{
+//     render(){
+//         return(
+//         <li>
+//             <label>
+//             <input type="checkbox"/>{this.props.value}
+//             </label>
+//         </li>
+//         );
+//     }
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import App from './AppTest';
 
+let data = ['Sample','Sample2'];
+
+let state_value = {
+  title:    'React',
+  data:     data,
+}
+
+function todoList( state = state_value, action ){
+  switch( action.type ){
+    case 'ADD':
+      return addReduce( state, action );
+    case 'DELETE':
+      return {
+        title: 'TEST2',
+        data: data,
+      };
+    default:
+      return state;
+  }
+}
+
+//アクションクリエーター
+function addReduce( state , action ){
+  return {
+    title: 'TEST1',
+    data:   data,
+  }
+}
+
+//ストアを作成
+let store = createStore(todoList);
+
+//レンダリング
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
