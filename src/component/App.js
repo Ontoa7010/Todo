a9f04d9b0ac792d5102ce2dc1a240a963999c70c
minase <ontoa7010@gmail.com>
2020-10-15 17:13:21 +0900

    CSSでヘッダーを作成 削除を実行できるようにした

--- src/component/App.js
+++ src/component/App.js
@@ -1,45 +1,35 @@
 import React , { useReducer , useState , useEffect } from 'react';
 
 import reducer ,{ addTodo } from '../reducers';
-import { ReadDatabase } from './Data';
+import Header from './Header';
+import Form from './Form';
+import Event from './Event';
+import { readDocument } from './Data';
 
 import '../css/App.css';
 
 const App = () => {
     const [ state , dispatch ] = useReducer( reducer , [] ); 
-    const [ item , setItem ] = useState('');
-
-    const doChange = e => {
-        e.preventDefault();
-        setItem(e.target.value);
-    }
-
-    const doClick = e =>{
-        e.preventDefault();
-        dispatch( addTodo(item) );
-    }
 
     //データベースからデータ読み込んでステートに保存
     useEffect( ()=>{
-        ReadDatabase({ state, dispatch} );
+        readDocument({ dispatch });
     }, [] );
 
     return (
         <div>
-            <h1>TodoList</h1>
-            <form>
-                <input type="text" id="addTodo" onChange={doChange}/>
-                <input type="submit" value="追加" onClick={doClick}/>
-            </form>
+            <Header />
+            <Form dispatch={dispatch}/>
             <ul>
                 { state.map( (event , index) => {
                     return(
-                    <li  key={index}><input type="checkbox"/>{event.item}<button>削除</button></li>
+                        <Event key={index} event={event} dispatch={dispatch}/>
                     );
                 })}
             </ul>
         </div>
     );
 }
+
 
 export default App;
